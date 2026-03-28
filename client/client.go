6ef9fbae61fe0f9900bd9551ab4cdc9afec0b23f
Piraya En-on - 6609650087 <piraya.en-@dome.tu.ac.th>
2026-03-28 13:44:10 +0700

    student list json

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"time"
 
@@ -12,11 +13,11 @@ import (
 
 func main() {
 
+	// Connect to gRPC server
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Did not connect: %v", err)
 	}
-
 	defer conn.Close()
 
 	client := pb.NewStudentServiceClient(conn)
@@ -24,8 +25,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	// GetStudent
-	res, err := client.GetStudent(ctx, &pb.StudentRequest{Id: 101})
+	// ---------------- GetStudent ----------------
+	res, err := client.GetStudent(ctx, &pb.StudentRequest{Id: 1})
 	if err != nil {
 		log.Fatalf("Error calling GetStudent: %v", err)
 	}
@@ -37,22 +38,23 @@ func main() {
 	log.Println("Email:", res.Email)
 	log.Println("Phone:", res.Phone)
 
-	// ListStudents
-	log.Println("\nStudent List:")
+	// ---------------- ListStudents ----------------
+	log.Println("\nStudent List (JSON):")
 
 	listRes, err := client.ListStudents(ctx, &pb.Empty{})
 	if err != nil {
 		log.Fatalf("Error calling ListStudents: %v", err)
 	}
 
-	for _, s := range listRes.Student {
-
-		log.Println("ID:", s.Id)
-		log.Println("Name:", s.Name)
-		log.Println("Major:", s.Major)
-		log.Println("Email:", s.Email)
-		log.Println("Phone:", s.Phone)
-		log.Println("------")
+	// Convert to JSON format
+	data := map[string]interface{}{
+		"students": listRes.Student,
 	}
 
+	jsonData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		log.Fatalf("Error converting to JSON: %v", err)
+	}
+
+	log.Println(string(jsonData))
 }
